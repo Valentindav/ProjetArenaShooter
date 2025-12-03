3082df50ea501d61e9729081737cd9c2c99b2873
Valentindav <vdavid@gaming.tech>
2025-12-03 18:29:56 +0100

    added weapon + parent

--- gce-dev-GC-ENGINE-Usable (1)/gce-dev-GC-ENGINE-Usable/src/Gameplay/main.cpp
+++ gce-dev-GC-ENGINE-Usable (1)/gce-dev-GC-ENGINE-Usable/src/Gameplay/main.cpp
@@ -3,38 +3,11 @@
 #include "Player.h"
 
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow) {	
+    //----------------------------------INIT WORLD----------------------------------
     gce::Console::Init();
     gce::GameManager::Create();    
 
     gce::Scene& scene = gce::Scene::Create();
-
-	gce::GameObject& CameraObject = gce::GameObject::Create(scene);
-    CameraObject.transform.LocalTranslate({ 0,0, -10 });
-    gce::Camera* pCamera = CameraObject.AddComponent<gce::Camera>();
-    pCamera->SetMainCamera();
-    pCamera->SetType(gce::PERSPECTIVE);
-    // Fov de base ici pi/4
-    pCamera->perspective.fov = XM_PIDIV4;
-    // point le plus proche que la camera verra
-    pCamera->perspective.nearPlane = 0.001f;
-    // point le plus eloigne que la camera verra
-    pCamera->perspective.farPlane = 500.0f;
-    // aspect ratio set en fonction de la largeur et hauteur de la fenetre
-    pCamera->perspective.aspectRatio = 1000.0f / 800.0f;
-    // ici on definis que l'axe Y va gerer la hauteur 
-    pCamera->perspective.up = { 0.0f, 1.0f, 0.0f };
-	Player* player = new Player(&CameraObject);
-
-    GameObject& testObject = GameObject::Create(scene);
-
-    MeshRenderer* pMeshRenderer = testObject.AddComponent<MeshRenderer>();
-    // SHAPES contient plusieurs shapes de base (je vous laisse regarder) pour faire des formes de bases
-    pMeshRenderer->SetGeometry(SHAPES.CUBE);
-
-    Texture* pNewTexture = new Texture("res/Exemple/TexturesTest.jpg");
-    pMeshRenderer->SetAlbedoTexture(pNewTexture);
-
-    testObject.transform.SetWorldPosition({ -2.0f,3.0f,0.0f });
 
     gce::WindowParam params;
     params.title = L"GCE Engine Window";
@@ -43,7 +16,50 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmd
     params.isFullScreen = false;
     params.isSplitScreen = false;
     params.screenDisposition = gce::SplitScreenDisposition::SQUARE_4_PLAYERS;
+
+    //----------------------------------INIT Camera----------------------------------
+	gce::GameObject& CameraObject = gce::GameObject::Create(scene);
+    CameraObject.transform.LocalTranslate({ 0,0, -10 });
+    gce::Camera* pCamera = CameraObject.AddComponent<gce::Camera>();
+    pCamera->SetMainCamera();
+    pCamera->SetType(gce::PERSPECTIVE);
+    pCamera->perspective.fov = XM_PIDIV4;
+    pCamera->perspective.nearPlane = 0.001f;
+    pCamera->perspective.farPlane = 500.0f;
+    pCamera->perspective.aspectRatio = 1000.0f / 800.0f;
+    pCamera->perspective.up = { 0.0f, 1.0f, 0.0f };
+
+    //----------------------------------Creating GameObject----------------------------------
+    GameObject& PlayerObject = GameObject::Create(scene);
+    MeshRenderer* pPlayerRenderer = PlayerObject.AddComponent<MeshRenderer>();
+    pPlayerRenderer->SetGeometry(SHAPES.CUBE);
+    Texture* pPlayerTexture = new Texture("res/Exemple/TexturesTest.jpg");
+    pPlayerRenderer->SetAlbedoTexture(pPlayerTexture);
+
+    GameObject& testObject = GameObject::Create(scene);
+    MeshRenderer* pMeshRenderer = testObject.AddComponent<MeshRenderer>();
+    pMeshRenderer->SetGeometry(SHAPES.CUBE);
+    Texture* pNewTexture = new Texture("res/Exemple/TexturesTest.jpg");
+    pMeshRenderer->SetAlbedoTexture(pNewTexture);
+
+    GameObject& Weapon = GameObject::Create(scene);
+    Weapon.transform.SetWorldPosition({ 0.0f,0.0f,0.0f });
+    MeshRenderer* pWeaponRenderer = Weapon.AddComponent<MeshRenderer>();
+    pWeaponRenderer->SetGeometry(GeometryFactory::LoadGeometry("res/Exemple/SUZANNE.obj"));
+    Texture* pWeaponTexture = new Texture("res/Exemple/TexturesTest.jpg");
+    pWeaponRenderer->SetAlbedoTexture(pWeaponTexture);
+    Weapon.transform.LocalScale({ 0.25,0.25,0.25 });
+
+    //----------------------------------Run----------------------------------
+    testObject.transform.SetWorldPosition({ -2.0f,3.0f,0.0f });
+    PlayerObject.transform.SetWorldPosition({ 0.0f,0.0f,-10.0f });
+    Weapon.transform.SetWorldPosition({ 1.0f,0.0f,-8.0f });
+
+	Player* player = new Player(&PlayerObject);
+    player->GetGameObject()->AddChild(CameraObject);
+    player->GetGameObject()->AddChild(Weapon);
 	player->AddMove();
+
     
     gce::GameManager::Run(params);	
 
