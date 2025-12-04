d9e30478de33b3916c1d12c6faf8582752bebb3f
YannisPoder <ypoder@gaming.tech>
2025-12-04 07:26:53 +0100

    fix z

--- gce-dev-GC-ENGINE-Usable (1)/gce-dev-GC-ENGINE-Usable/src/Gameplay/Player.cpp
+++ gce-dev-GC-ENGINE-Usable (1)/gce-dev-GC-ENGINE-Usable/src/Gameplay/Player.cpp
@@ -1,4 +1,5 @@
 #include "Player.h"
+#include <cmath>
 
 using namespace gce;
 
@@ -6,67 +7,57 @@ DECLARE_SCRIPT(Move, ScriptFlag::Update)
 
 void Update()
 {
-	gce::LockMouseCursor();
-	GameObject* obj = m_pOwner;
-	if (GetKey(Keyboard::Z)) {
-		obj->transform.WorldTranslate(obj->transform.GetLocalForward() * 2 * GameManager::DeltaTime());
-	}
-	if (GetKey(Keyboard::S)) {
-		obj->transform.WorldTranslate(obj->transform.GetLocalForward() * (-2) * GameManager::DeltaTime());
-	}
-	if (GetKey(Keyboard::Q)) {
-		obj->transform.WorldTranslate(obj->transform.GetLocalRight() * (-2) * GameManager::DeltaTime());
-	}
-	if (GetKey(Keyboard::D)) {
-		obj->transform.WorldTranslate(obj->transform.GetLocalRight() * 2 * GameManager::DeltaTime());
-	}
-	if (GetKey(Keyboard::SPACE)) {
-		obj->transform.WorldTranslate(obj->transform.GetLocalUp() * 2 * GameManager::DeltaTime());
-	}
-	if (GetKey(Keyboard::LCTRL)) {
-		obj->transform.WorldTranslate(obj->transform.GetLocalUp() * (-2) * GameManager::DeltaTime());
-	}
-	gce::WindowParam windowParam = GameManager::GetWindowParam();
-	gce::Vector2i32 const center = { windowParam.width / 2, windowParam.height / 2 };
-	gce::Vector2i32 const currentPos = GetMousePosition();
-	gce::Vector2f32 const deltaPixels = currentPos - center;
+    gce::LockMouseCursor();
+    GameObject* obj = m_pOwner;
+    if (GetKey(Keyboard::Z)) {
+        obj->transform.WorldTranslate(obj->transform.GetLocalForward() * 2 * GameManager::DeltaTime());
+    }
+    if (GetKey(Keyboard::S)) {
+        obj->transform.WorldTranslate(obj->transform.GetLocalForward() * (-2) * GameManager::DeltaTime());
+    }
+    if (GetKey(Keyboard::Q)) {
+        obj->transform.WorldTranslate(obj->transform.GetLocalRight() * (-2) * GameManager::DeltaTime());
+    }
+    if (GetKey(Keyboard::D)) {
+        obj->transform.WorldTranslate(obj->transform.GetLocalRight() * 2 * GameManager::DeltaTime());
+    }
+    if (GetKey(Keyboard::SPACE)) {
+        obj->transform.WorldTranslate(obj->transform.GetLocalUp() * 2 * GameManager::DeltaTime());
+    }
+    if (GetKey(Keyboard::LCTRL)) {
+        obj->transform.WorldTranslate(obj->transform.GetLocalUp() * (-2) * GameManager::DeltaTime());
+    }
 
-	static float yaw = 0.0f;
-	static float pitch = 0.0f;
-	static float prevYaw = 0.0f;
-	static float prevPitch = 0.0f;
+    gce::WindowParam windowParam = GameManager::GetWindowParam();
+    gce::Vector2i32 const center = { windowParam.width / 2, windowParam.height / 2 };
+    gce::Vector2i32 const currentPos = GetMousePosition();
+    gce::Vector2f32 const deltaPixels = currentPos - center;
 
-	const float sensitivity = 0.0005f;
-	const float pitchMin = -1.4f;
-	const float pitchMax = 1.4f;
+    static float yaw = 0.0f;
+    static float pitch = 0.0f;
 
-	float yawDelta = deltaPixels.x * sensitivity;
-	float pitchDelta = deltaPixels.y * sensitivity;
+    const float sensitivity = 0.0005f;
+    const float pitchMin = -1.4f;
+    const float pitchMax = 1.4f;
 
-	yaw += yawDelta;
-	pitch += pitchDelta;
-	
-	pitch = gce::Clamp(pitch, pitchMin, pitchMax);
+    yaw += deltaPixels.x * sensitivity;
+    pitch += deltaPixels.y * sensitivity;
+    pitch = gce::Clamp(pitch, pitchMin, pitchMax);
 
-	float applyPitch = pitch - prevPitch;
-	float applyYaw = yaw - prevYaw;
+    Quaternion quaternion = Quaternion::RotationEuler(pitch, yaw, 0.0f);
+    obj->transform.SetLocalRotation(quaternion);
 
-	obj->transform.LocalRotate({ applyPitch, applyYaw, 0.0f });	
-
-	prevPitch = pitch;
-	prevYaw = yaw;
-
-	SetMousePosition(center);
+    SetMousePosition(center);
 }
 
 END_SCRIPT
 
-void Player::MovePlayer() {		
-	
+void Player::MovePlayer() {
+
 }
 
 void Player::AddMove()
 {
-	GameObject* obj = GetGameObject();
-	obj->AddScript<Move>();
+    GameObject* obj = GetGameObject();
+    obj->AddScript<Move>();
 }
